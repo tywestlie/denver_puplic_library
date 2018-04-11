3fdfe79edede38622c63c760b5d50b47e2facf8a
Tyler Westlie <tyler.westlie@gmail.com>
2018-04-11 09:22:39 -0600

    iteration 1 complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,24 @@
 class Book
+  attr_reader :books
 
+  def initialize(books)
+    @books = books
+  end
 
+  def author_first_name
+    @books[:author_first_name]
+  end
+
+  def author_last_name
+    @books[:author_last_name]
+  end
+
+  def title
+    @books[:title]
+  end
+
+  def publication_date
+    @books[:publication_date].split.last
+    require 'pry'; binding.pry
+  end
 end
--- test/book_test.rb
+++ test/book_test.rb
@@ -1,6 +1,33 @@
 require './test/test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/book'
 
 class BookTest < Minitest::Test
 
+  def test_exist
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
 
+    assert_instance_of Book, book
+  end
+
+  def test_author_first_name
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    assert_equal "Harper", book.author_first_name
+  end
+
+  def test_author_last_name
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    assert_equal "Lee", book.author_last_name
+  end
+
+  def test_title
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    assert_equal "To Kill a Mockingbird", book.title
+  end
+
+  def test_publication_date
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    assert_equal "1960", book.publication_date
+  end
 end
